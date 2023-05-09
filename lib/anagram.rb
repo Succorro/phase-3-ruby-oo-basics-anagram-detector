522b8a5cb461bd25c6b4dd01eff18c70c474e3af
Steven Gutierrez <stevogbmv@gmail.com>
2023-05-09 14:54:56 -0700

    commit tree 3eb7bf93dc46deb31a6927525ec7672ed5b9b606

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+# recieve an array of strings 
+# match Anagram class to new word to listen for
+# after listening word is created, 
+# input new array of strings and output the single word
+#  from the array that matches the anagram of the 
+# listening word
+require 'pry'
+class Anagram
+    attr_accessor :arr
+    def initialize(word = 'lleho')
+        @word = word
+    end
+    def match(arr = %w[yellow lleho])
+      arr.filter{|word| word.chars.sort == @word.chars.sort}
+    end
+   
+
+end
